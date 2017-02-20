2501d7b17bb1cf848a6b977f11ad3c379b510681
ashfaq <ashfaqpatwari@gmail.com>
2017-02-20 23:13:18 +0300

    register page design

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController, LoadingController, Loading } from 'ionic-angular';
+
+import { LoginPage } from '../login/login';
+import { HomepagePage } from '../homepage/homepage';
 
 /*
   Generated class for the Register page.
@@ -12,11 +15,41 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html'
 })
 export class RegisterPage {
+  
+  loading: Loading;
+  registerCredentials = {
+    driverName: '',
+    driverUsername: '',
+    driverId: '',
+    driverPassword: '',
+    driverConfirmPassword: '',
+    adminPassword: '',
+    apiUrl: ''
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+   private loadingCtrl: LoadingController, private toastCtrl: ToastController) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  goTologin() {
+    this.navCtrl.setRoot(LoginPage);
+  }
+
+  register() {
+    this.loading = this.loadingCtrl.create();
+    this.loading.present();
+    setTimeout(() => {
+      this.loading.dismiss();
+      let toast = this.toastCtrl.create({
+        message: `New Driver ${this.registerCredentials.driverName} was added successfully`,
+        duration: 3000
+      });
+      toast.present();
+      this.navCtrl.setRoot(HomepagePage);
+    }, 3000);
   }
 
 }
